1af80e5c6989b5c6fd71f397909f5b411970d5ff
Engineering <engineering@refactory.id>
2022-08-06 15:11:38 +0700

    init project with tailwind and abosulte path

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <LandingPage/>
+</template>
+
+<script setup>
+import LandingPage from '@pages/LandingPage.vue';
+</script>
+
+<style scoped>
+</style>
---
+++ src/pages/LandingPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <p class="text-green-500">Halaman Landing Page</p>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
